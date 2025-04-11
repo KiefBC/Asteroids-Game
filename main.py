cf8e0662d0aca251528206f3618fdb02cb4d7400
KiefBC <kiefhay642@outlook.com>
2025-04-10 22:54:19 -0700

    Intital Commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+import player as p1
 
 def main():
     print("Starting Asteroids!")
@@ -8,14 +9,27 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH // 2
+    y = SCREEN_HEIGHT // 2
+    player = p1.player(x, y)
+
 
     while True:
+        # Enables the Close Window Button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
         screen.fill((0, 0, 0))
-        pygame.display.flip()
+        player.draw(screen)
+        pygame.display.flip() # This line updates the display
+
+        # Limit the frame rate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
